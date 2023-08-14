4ed740aeb791dcb0c1f62ecffe8c55244fe770ed
kiran madiwalar <kiranm81040@gmail.com>
2023-08-14 20:53:32 +0530

    completed

--- script.js
+++ script.js
@@ -1,32 +1,48 @@
 var cluster = "";
 var time = 60;
-var target = Math.floor(Math.random() * 10);
-var score;
+var target;
+var score = 0;
 
 function createBubbles() {
-  for (var i = 1; i <= 70; i++) {
+  for (var i = 1; i <= 147; i++) {
     var randomNum = Math.floor(Math.random() * 10);
     cluster += `<div class="full-bubbles">${randomNum}</div> `;
-    document.querySelector(".full-bubbles").innerHTML = cluster;
+    document.querySelector(".bubbles").innerHTML = cluster;
   }
 }
-createBubbles();
 
 function runTime() {
-  setInterval(() => {
+  var timeinterval = setInterval(() => {
     if (time > 0) {
       time--;
       document.querySelector(".time").textContent = time;
+    } else {
+      clearInterval(timeinterval);
+      document.querySelector(
+        ".bubbles"
+      ).innerHTML = `<h1>Game Over and Your Score is ${score} </h1>`;
     }
-  }, 100);
+  }, 1000);
 }
-runTime();
 
 function setTarget() {
+  target = Math.floor(Math.random() * 10);
   document.querySelector(".target").textContent = target;
 }
-setTarget();
-
-function setScore() {
-  document.querySelector(".full-bubbles").textContent = events;
+function increaseScore() {
+  score += 10;
+  document.querySelector(".score").innerHTML = score;
 }
+document.querySelector(".bubbles").addEventListener("click", (e) => {
+  var clickedNum = Number(e.target.textContent);
+  if (clickedNum === target) {
+    increaseScore();
+    setTarget();
+    cluster = "";
+    createBubbles();
+  }
+});
+
+createBubbles();
+runTime();
+setTarget();
